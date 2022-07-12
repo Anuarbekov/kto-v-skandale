ac7393c71c3eddd0a6f973915476b66a4e7fbf27
Meir <mikonek.a@gmail.com>
2022-07-13 03:50:20 +0600

    first

---
+++ src/components/InfoCard.js
@@ -0,0 +1,29 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+const InfoCard = ({ text, level }) => {
+  return (
+    <Card sx={{ maxWidth: 1000, marginTop: 5, marginLeft: 4, marginRight: 4 }}>
+      <CardContent>
+        <Divider
+          sx={{ marginTop: 1, fontFamily: "Montserrat", fontWeight: "bold", fontSize: 18 }}
+        >
+          Ваш уровень: {level}
+        </Divider>
+        <Typography
+          variant="body2"
+          sx={{
+            fontSize: 16,
+            marginTop: 2,
+            fontFamily: "Montserrat",
+            fontWeight: "bold",
+          }}
+        >
+          {text}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+export default InfoCard;
